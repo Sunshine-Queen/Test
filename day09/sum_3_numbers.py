2e2e0502c1c3ed98bd8e2b746128ae757c607d20
Sunshine-Queen <1053710934@qq.com>
2018-11-21 19:56:28 +0800

    第九天函数的嵌套

---
+++ day09/sum_3_numbers.py
@@ -0,0 +1,10 @@
+def sum_3_nums(a,b,c):
+    result=a+b+c
+    print("%d+%d+%d=%d"%(a,b,c,result))
+
+#1.获取三个数值
+num1=int(input("第一个值："))
+num2=int(input("第二个值："))
+num3=int(input("第三个值："))
+
+sum_3_nums(num1,num2,num3)
---
+++ day09/函数的嵌套调用.py
@@ -0,0 +1,15 @@
+def testB():
+    print('---- testB start----')
+    print('这里是testB函数执行的代码...(省略)...')
+    print('---- testB end----')
+
+
+def testA():
+    print('---- testA start----')
+
+    testB()
+
+    print('---- testA end----')
+
+
+testA()
